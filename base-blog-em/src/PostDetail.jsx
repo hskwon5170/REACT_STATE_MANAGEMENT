c58eb984c3a9aca3be6ccb3cd000e9baf9b6823c
hskwon5170 <hskwon517@gmail.com>
2022-10-24 16:07:13 +0900

    각 게시물에 대한 댓글 fetch 위해 query key 변경

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,3 +1,4 @@
+import { useQuery } from "react-query";
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
@@ -23,15 +24,25 @@ async function updatePost(postId) {
 
 export function PostDetail({ post }) {
   // replace with useQuery
-  const data = [];
-
+  const { data, isError, error, isLoading } = useQuery(
+    ["commentsssssss", post.id],
+    () => fetchComments(post.id)
+  );
+  if (isLoading) return <h3>로딩중입니다. 잠시만 기다려주세요</h3>;
+  if (isError)
+    return (
+      <div>
+        <h3>Error</h3>
+        <p>{error.toString()}</p>
+      </div>
+    );
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
       <button>Delete</button> <button>Update title</button>
       <p>{post.body}</p>
       <h4>Comments</h4>
-      {data.map((comment) => (
+      {data.map(comment => (
         <li key={comment.id}>
           {comment.email}: {comment.body}
         </li>
