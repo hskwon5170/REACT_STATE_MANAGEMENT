868fc9960b0ed12dfff811e84f403f71ee9f3c01
hskwon5170 <hskwon517@gmail.com>
2022-10-25 15:41:58 +0900

    add prefetchQuery(treatments)

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query';
+import { useQuery, useQueryClient } from 'react-query';
 
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
@@ -28,3 +28,10 @@ export function useTreatments(): Treatment[] {
   return data;
 }
 // 각각의 컴포넌트마다 error handling을 해주는게 아니라 react-query/queryClient(이것을 app.tsx에서 불러옴)에서 중앙집중식으로 해줄 수 있음
+
+export function usePrefetchTreatments(): void {
+  const queryClient = useQueryClient();
+  queryClient.prefetchQuery(queryKeys.treatments, getTreatments);
+}
+// treatments에 대한 내용을 캐싱해서 프리페칭 하기 위한 코드
+// 메인 홈에 접속했음에도 불구하고 Treatments에 대한 내용들을 미리 로드해놓는다
