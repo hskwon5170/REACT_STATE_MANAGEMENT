398ddb3e92c96399a1515f9423419c2d5d41bf37
hskwon5170 <hskwon517@gmail.com>
2022-10-25 15:26:50 +0900

    add global Error handling

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -3,7 +3,6 @@ import { useQuery } from 'react-query';
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
-import { useCustomToast } from '../../app/hooks/useCustomToast';
 
 // for when we need a query function for useQuery
 async function getTreatments(): Promise<Treatment[]> {
@@ -13,6 +12,19 @@ async function getTreatments(): Promise<Treatment[]> {
 
 export function useTreatments(): Treatment[] {
   const fallback = [];
-  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
+  const { data = fallback } = useQuery(
+    queryKeys.treatments,
+    getTreatments,
+    // {
+    // onError: (error) => {
+    //   const title =
+    //     error instanceof Error
+    //       ? error.message
+    //       : 'error connecting to the server';
+    //   toast({ title, status: 'error' });
+    // },
+    // }
+  );
   return data;
 }
+// 각각의 컴포넌트마다 error handling을 해주는게 아니라 react-query/queryClient(이것을 app.tsx에서 불러옴)에서 중앙집중식으로 해줄 수 있음
